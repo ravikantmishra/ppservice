f08224a8cfe138bebefb14d0df99a63ae900122f
Raman <raman@administrator.(none)>
2013-09-05 18:12:03 +0530

    completed the registration and login functionality

---
+++ module/User/config/module.config.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * Organization: OSSCube
+* Added: Ramandeep Singh
+* Scope: User module route page
+* Dated: 05-09-2013
+*/
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'User\Controller\User' => 'User\Controller\UserController',
+        ),
+    ),
+		'router' => array(
+				'routes' => array(
+						'login' => array(
+								'type'    => 'segment',
+								'options' => array(
+										'route'    => '/login',										
+										'defaults' => array(
+												'controller' => 'User\Controller\User',
+												'action'     => 'login',
+										),
+								),
+						),
+						'register' => array(
+								'type'    => 'segment',
+								'options' => array(
+										'route'    => '/register',
+										'defaults' => array(
+												'controller' => 'User\Controller\User',
+												'action'     => 'register',
+										),
+								),
+						),
+				),
+		),
+		
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'User' => __DIR__ . '/../view',
+        ),
+    ),
+);
