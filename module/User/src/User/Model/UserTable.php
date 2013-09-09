046809c41d924c530ede3bd0a01240811dcf5eae
Raman <raman@administrator.(none)>
2013-09-09 15:25:19 +0530

    ajax email verification on register form

--- module/User/src/User/Model/UserTable.php
+++ module/User/src/User/Model/UserTable.php
@@ -17,36 +17,49 @@ use Zend\Db\Sql\Where;
 
 class UserTable {
 	protected $tableGateway;
+	
 	public function __construct(TableGateway $tableGateway) {
 		$this->tableGateway = $tableGateway;
 	}
 
-	public function saveUser($data) {	
+	//Function to insert value in user table
+			//-->requires an multidimentioanal array 
+	public function saveUser($data) {
 		$this->tableGateway->insert ( $data );
 		$lastId =  $this->tableGateway->lastInsertValue;
 		 
 		return $lastId;		
 	}
 	
+	//Function to set the session
+			//-->require key regarding which the session has to be created and value
 	public function setUserSession($key,$value) {
 		$userSession = new Container('user');
 		$userSession[$key] = $value;
 	}
 	
+	//Function to fetch recordes from user table
+			//-->require an array for where condition
 	public function getUser($where) {
-		$row = $this->tableGateway->select ( $where )->current();
+		$row = $this->tableGateway->select ( $where );
 		if (! $row) {
 			return false;
 		} else {
-			return $row;
+			foreach ($row as $key=>$val){
+				$result[$key]=(array)$val;
+			}
+			return $result;
 		}
 	}
 	
+	// Function to update user
+			//--> require two multidimentional array first for value to be updated second for where clause 
 	public function updateUser($value,$where) {
 		
 		$this->tableGateway->update( $value, $where );
 	}
 
+	// Fucntion to valid the user at the time of login
 	public function validateUser(LoginEntity $login) {
 		
 		$where = new Where();
